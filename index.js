824960c4ce91d6e389605317caa042b4f7b73e09
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-10-04 15:39:30 +0500

    Test JWT

--- index.js
+++ index.js
@@ -1,4 +1,28 @@
 import express from "express"
+import jwt from "jsonwebtoken"
+
+const users = {
+	bro: "1234",
+	ali: "nolbeshellibirnol",
+	rano: "1234",
+}
+
+const secret = "1234"
+
+const payloadForBro = {
+	username: "bro",
+	isAdmin: true,
+}
+
+const token = jwt.sign( payloadForBro, secret, { expiresIn: 10 } )
+
+console.log( "Imm.", jwt.verify( token, secret ) )
+
+setTimeout( () => {
+
+	console.log( "Aft. 15 sec.", jwt.verify( token, secret ) )
+
+}, 15_000 )
 
 const app = express()
 
