3c1d2d711f8ba8d6064f1344b9ab4b220c839abe
Muhammadxon Najimov <129692531+najimovs@users.noreply.github.com>
2025-10-04 16:21:10 +0500

    Implement mock login

--- index.js
+++ index.js
@@ -4,29 +4,66 @@ import jwt from "jsonwebtoken"
 const users = {
 	bro: "1234",
 	ali: "nolbeshellibirnol",
-	rano: "1234",
+	rano: "r",
 }
 
-const secret = "1234"
+// const secret = "1234"
 
-const payloadForBro = {
-	username: "bro",
-	isAdmin: true,
-}
+// const payloadForBro = {
+// 	username: "bro",
+// 	isAdmin: true,
+// }
 
-const token = jwt.sign( payloadForBro, secret, { expiresIn: 10 } )
+// const token = jwt.sign( payloadForBro, secret, { expiresIn: 10 } )
 
-console.log( "Imm.", jwt.verify( token, secret ) )
+// console.log( "Imm.", jwt.verify( token, secret ) )
 
-setTimeout( () => {
+// setTimeout( () => {
 
-	console.log( "Aft. 15 sec.", jwt.verify( token, secret ) )
+// 	console.log( "Aft. 15 sec.", jwt.verify( token, secret ) )
 
-}, 15_000 )
+// }, 15_000 )
 
 const app = express()
 
+app.use( express.json() )
+
 const secretData = [ 1, 2, 3, 4, 5 ]
+
+app.post( "/login", ( req, res ) => {
+
+	const { username, password } = req.body
+
+	if ( !username || !password ) {
+
+		res.status( 400 ).send( { message: "username and password MUST be included in body" } )
+
+		return
+	}
+
+	// if ( users[ username ] === undefined || users[ username ] !== password ) {
+
+	// 	res.status( 401 ).send( { message: "wrong username or password" } )
+
+	// 	return
+	// }
+
+	if ( users[ username ] === undefined ) {
+
+		res.status( 401 ).send( { message: "username not found" } )
+
+		return
+	}
+
+	if ( users[ username ] !== password ) {
+
+		res.status( 401 ).send( { message: "wrong password" } )
+
+		return
+	}
+
+	res.send( { message: "OK" } )
+} )
 
 app.get( "/secret-data", ( req, res ) => {
 
